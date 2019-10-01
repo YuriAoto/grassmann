2aa059ca687f5285c7df78a9478091e3c6ffd720
Yuri Alexandre Aoto <yuri.aoto@ufabc.edu.br>
2019-10-01 16:25:48 -0300

    Improve ABC for wave functions

--- dGr_general_WF.py
+++ dGr_general_WF.py
@@ -5,14 +5,16 @@ Abstract base class for electronic wave functions.
 """
 import logging
 from collections import namedtuple
-from collections.abc import Sequence, Mapping, Collection
+from collections.abc import Sequence, Mapping, Sized, Iterable, Container
 from abc import ABC, abstractmethod
 
 import numpy as np
 
+from dGr_exceptions import *
+
 logger = logging.getLogger(__name__)
 
-class Spirrep_String_Index(Collection):
+class Spirrep_String_Index(Sized, Iterable, Container):
     """An string index for a single spirrep
     
     Attributes:
@@ -24,12 +26,12 @@ class Spirrep_String_Index(Collection):
     occ_orb (np.ndarray)
         An numpy array of int8 with the occupied orbitals
     """
-    __slots__ = ('I_spirreps',
+    __slots__ = ('standard_position_of_string',
                  'occ_orb')
     
     def __init__(self, n_elec):
         self.standard_position_of_string = 0
-        self.occ_orb = np.zeros(n_elec, dtype=np.int8)
+        self.occ_orb = np.arange(n_elec, dtype=np.int8)
     
     def __contains__(self, x):
         return x in self.occ_orb
@@ -37,11 +39,21 @@ class Spirrep_String_Index(Collection):
     def __iter__(self):
         return self.occ_orb
     
+    def __setitem__(self, key, value):
+        self.occ_orb.__setitem__(key, value)
+    
     def __len__(self):
         return len(self.occ_orb)
     
     def __int__(self):
         return self.standard_position_of_string
+    
+    def __repr__(self):
+        return ('I = ' + str(self.standard_position_of_string) + ': '
+                + str(self.occ_orb))
+    
+    def __str__(self):
+        return (str(self.occ_orb))
 
 class String_Index(Mapping):
     """A full string index, for all spirreps
@@ -66,8 +78,20 @@ class String_Index(Mapping):
     def __getitem__(self, spirrep):
         return self.spirrep_indices[spirrep]
     
+    def __setitem__(self, key, value):
+        self.spirrep_indices.__setitem__(key, value)
+    
     def __len__(self):
         return len(self.spirrep_indices)
+    
+    def append(self, value):
+        self.spirrep_indices.append(value)
+    
+    def __repr__(self):
+        return '\n'.join(map(str, self.spirrep_indices))
+    
+    def __str__(self):
+        return ' ^ '.join(map(str, self.spirrep_indices))
 
 
 class Wave_Function(ABC, Sequence):
@@ -82,10 +106,11 @@ class Wave_Function(ABC, Sequence):
     n_elec (int)                      number of electrons
     n_alpha (int)                     number of alpha electrons
     n_beta (int)                      number of beta electrons
-    n_frozen (int)                    number of frozen orbitals
+    n_core (int)                      number of core orbitals
                                        (assumed to be equal for alpha and beta)
     ref_occ (n_irrep-tuple of int)    occupation of the reference determinant
     WF_type (str)                     type of wave function
+    source (str)                      The source of this wave function
     
     Some rules about Sequence's abstract methods:
     
@@ -96,34 +121,122 @@ class Wave_Function(ABC, Sequence):
     """
     
     def __init__(self):
-        self.n_irrep = 0
-        self.n_strings = [0]
-        self.orb_dim = []
-        self.n_elec = 0
-        self.n_alpha = 0
-        self.n_beta = 0
-        self.n_frozen = 0
-        self.ref_occ = []
-        self.WF_type = ''
+        self.restricted = None
+        self.point_group = None
+        self.n_irrep = None
+        self.n_core = None
+        self.n_act = None
+        self.orb_dim = None
+        self.ref_occ = None
+        self.n_alpha = None
+        self.n_beta = None
+        self.WF_type = None
+        self.source = None
     
     def __repr__(self):
         """Return string with parameters of the wave function."""
         x = []
         x.append('-'*50)
+        x.append('point group: {}'.format(self.point_group))
         x.append('n irrep: {}'.format(self.n_irrep))
         x.append('orb dim: {}'.format(self.orb_dim))
+        x.append('n core: {}'.format(self.n_core))
+        x.append('n act: {}'.format(self.n_act))
+        x.append('ref occ: {}'.format(self.ref_occ))
         x.append('n electrons: {}'.format(self.n_elec))
         x.append('n alpha: {}'.format(self.n_alpha))
         x.append('n beta: {}'.format(self.n_beta))
-        x.append('n frozen: {}'.format(self.n_frozen))
         x.append('WF type: {}'.format(self.WF_type))
+        x.append('source: {}'.format(self.source))
         x.append('-'*50)
         return '\n'.join(x)
+    
+    def initialize_data(self):
+        if self.point_group is None:
+            raise dGrValueError('I still do not know the point group!')
+        if self.restricted is None:
+            raise dGrValueError('I still do not know if it is a restricted'
+                                + ' or unrestricted wave function!')
+        self.n_alpha = self.n_beta = 0
+        self.orb_dim = np.zeros(self.n_irrep,
+                                dtype=np.uint8)
+        self.ref_occ = np.zeros(self.n_irrep * (1 if self.restricted else 2),
+                                dtype=np.uint8)
+        self.n_core = np.zeros(self.n_irrep,
+                               dtype=np.uint8)
+        self.n_act = np.zeros(self.n_irrep,
+                              dtype=np.uint8)
+        self.n_strings = np.zeros(self.n_irrep * (1 if self.restricted else 2),
+                                  dtype=np.uint8)
     
     def spirrep_blocks(self):
         """Yield the possible spin and irreps, as a single integer."""
         for spirrep in range(2 * n_irrep):
             yield spirrep
+    
+    @property
+    def n_elec(self):
+        if self.n_alpha is None or self.n_beta is None:
+            return None
+        else:
+            return self.n_alpha + self.n_beta
+    
+    @property
+    def n_corr_alpha(self):
+        if (self.n_alpha is None
+            or self.n_corr_orb is None):
+            return None
+        else:
+            return self.n_alpha - sum(self.n_core[:self.n_irrep])
+    
+    @property
+    def n_corr_beta(self):
+        if (self.n_beta is None
+            or self.n_corr_orb is None):
+            return None
+        else:
+            if self.restricted:
+                return self.n_corr_alpha
+            else:
+                return self.n_beta - sum(self.n_core[:self.n_irrep])
+    
+    @property
+    def n_corr_elec(self):
+        ncorr_a = self.n_corr_alpha
+        if restricted:
+            return (None
+                    if ncorr_a is None else
+                    2 * ncorr_a)
+        else:
+            ncorr_b = self.n_corr_alpha
+            return (None
+                    if (ncorr_a is None or ncorr_b is None) else
+                    ncorr_a + ncorr_b)
+    
+    @property
+    def n_ext(self):
+        if (self.orb_dim is None
+            or self.ref_occ is None
+            or self.n_act is None):
+            return None
+        else:
+            if self.restricted:
+                return self.orb_dim - self.ref_occ
+            else:
+                return np.concatenate((self.orb_dim,
+                                       self.orb_dim)) - self.ref_occ
+    
+    @property
+    def n_corr_orb(self):
+        if (self.ref_occ is None
+            or self.n_core is None):
+            return None
+        else:
+            if self.restricted:
+                return self.ref_occ - self.n_core
+            else:
+                return self.ref_occ - np.concatenate((self.n_core,
+                                                      self.n_core))
     
     @abstractmethod
     def string_indices(self, spirrep=None, coupled_to=None, no_occ_orb=False):
