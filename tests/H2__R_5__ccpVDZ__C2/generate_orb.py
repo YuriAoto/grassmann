ef4ff2530a94acae7b6579c4df424608e567a805
Yuri Alexandre Aoto <yuri.aoto@ufabc.edu.br>
2020-03-25 15:44:49 -0300

    Change tests for H2 cc-pVDZ

---
+++ tests/H2__R_5__ccpVDZ__C2/generate_orb.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+import numpy as np
+from scipy import linalg
+
+U = []
+
+U.append(np.zeros((5,1)))
+U.append(np.zeros((5,0)))
+
+
+U[0][0,0] = 0.33
+U[0][1,0] = 0.44
+U[0][2,0] = 0.24
+U[0][3,0] = 0.15
+U[0][4,0] = 0.74
+
+zero_skip_linalg = 1.0E-8
+
+for i, Ui in enumerate(U):
+    norm_Ui = linalg.norm(Ui)
+    if norm_Ui > zero_skip_linalg:
+        U[i] = linalg.orth(Ui)
+    print(U[i].T @ U[i])
+
+
+np.savez(os.path.dirname(os.path.abspath('__file__'))
+         + '/restricted_ini_U', *U)
+
+#for i in range(8):
+#    U.append(np.array(U[i]))
+
+#np.savez('unrestricted_ini_U', *U)
+
+exit()
---
+++ tests/H2__R_5__ccpVDZ__nosym/generate_orb.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+import numpy as np
+from scipy import linalg
+
+U = []
+
+U.append(np.zeros((10,1)))
+
+
+U[0][0,0] = 0.33
+U[0][1,0] = 0.44
+U[0][2,0] = -0.24
+U[0][3,0] = 0.15
+U[0][4,0] = -0.74
+U[0][5,0] = 0.24
+U[0][6,0] = 0.74
+U[0][7,0] = 0.67
+U[0][9,0] = -0.12
+
+zero_skip_linalg = 1.0E-8
+
+for i, Ui in enumerate(U):
+    norm_Ui = linalg.norm(Ui)
+    if norm_Ui > zero_skip_linalg:
+        U[i] = linalg.orth(Ui)
+    print(U[i].T @ U[i])
+
+
+np.savez(os.path.dirname(os.path.abspath('__file__'))
+         + '/restricted_ini_U', *U)
+
+#for i in range(8):
+#    U.append(np.array(U[i]))
+
+#np.savez('unrestricted_ini_U', *U)
+
+exit()
